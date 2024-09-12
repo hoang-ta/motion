924abf9de3d81089df8f5b403d09420c7eeaa8d8
Hoang Ta <huyhoang.ta97@gmail.com>
2024-09-12 22:11:57 +0700

    add animation for dynamic height

--- app/animation/layout/3/page.tsx
+++ app/animation/layout/3/page.tsx
@@ -1,10 +1,32 @@
-import { motion } from 'framer-motion';
+'use client';
+
+import { AnimatePresence, motion } from 'framer-motion';
 import { useState, useRef, useEffect } from 'react';
+import './styles.css';
 
 export default function Example() {
   const [showExtraContent, setShowExtraContent] =
     useState(false);
+  const ref = useRef(null);
+  const [height, setHeight] = useState('auto');
 
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        // console.log(
+        //   entry,
+        //   entry.contentBoxSize,
+        //   entry.contentBoxSize[0].blockSize,
+        // );
+        setHeight(entry.borderBoxSize[0].blockSize);
+      }
+    });
+    resizeObserver.observe(ref.current);
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+  //   console.log(height);
   return (
     <div className='wrapper'>
       <button
@@ -13,22 +35,39 @@ export default function Example() {
       >
         Toggle height
       </button>
-      <div className='element'>
-        <div className='inner'>
+      <motion.div
+        animate={{ height }}
+        style={{ height }}
+        className='element'
+      >
+        <div className='inner' ref={ref}>
           <h1>Fake Family Drawer</h1>
           <p>
             This is a fake family drawer. Animating height
             is tricky, but satisfying when it works.
           </p>
-          {showExtraContent ? (
-            <p>
-              This extra content will change the height of
-              the drawer. Some even more content to make the
-              drawer taller and taller and taller...
-            </p>
-          ) : null}
+          <AnimatePresence>
+            {showExtraContent ? (
+              <motion.p
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: {
+                    duration: 0.1,
+                  },
+                }}
+              >
+                This extra content will change the height of
+                the drawer. Some even more content to make
+                the drawer taller and taller and taller...
+              </motion.p>
+            ) : null}
+          </AnimatePresence>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
